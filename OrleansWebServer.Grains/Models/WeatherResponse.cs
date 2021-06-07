93bacc9c645660a08d06089d362e3cc124c812ff
devgopher <igevdo@mail.ru>
2021-06-07 16:49:30 +0300

    - WebRequest/response

---
+++ OrleansWebServer.Grains/Models/WeatherResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace OrleansWebServer.Grains.Models
+{
+    public class WeatherResponse
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+
+
+        public string GrainId { get; set; }
+}
+}
